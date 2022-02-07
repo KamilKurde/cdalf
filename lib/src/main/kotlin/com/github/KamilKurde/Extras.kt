a19ca60ac5bd9dbc38a0aaa79069e2d933b8fd08
Kamil Bąk <kamilbak113@gmail.com>
2022-02-07 08:15:31 +0100

    Changed classToEntry visibility

--- lib/src/main/kotlin/com/github/KamilKurde/Extras.kt
+++ lib/src/main/kotlin/com/github/KamilKurde/Extras.kt
@@ -2,7 +2,7 @@ package com.github.KamilKurde
 
 import kotlin.reflect.KClass
 
-data class Extras(val classToEntry: MutableMap<KClass<*>, MutableMap<String, Any?>> = mutableMapOf()) {
+data class Extras(private val classToEntry: MutableMap<KClass<*>, MutableMap<String, Any?>> = mutableMapOf()) {
 	companion object {
 		
 		val wrappedClasses = setOf<KClass<*>>(
@@ -18,16 +18,22 @@ data class Extras(val classToEntry: MutableMap<KClass<*>, MutableMap<String, Any
 		)
 	}
 	
-	inline fun <reified T> put(key: String, value: T) {
-		require(T::class.java.isPrimitive || T::class in wrappedClasses) { "value is required to be of primitive type, it is of ${T::class.qualifiedName} instead" }
-		val current = classToEntry[T::class] ?: mutableMapOf()
+	fun <T>put(kclass: KClass<*>, key: String, value: T)
+	{
+		require(kclass.java.isPrimitive || kclass in wrappedClasses) { "value is required to be of primitive type, it is of ${kclass.qualifiedName} instead" }
+		val current = classToEntry[kclass] ?: mutableMapOf()
 		current[key] = value
-		classToEntry[T::class] = current
+		classToEntry[kclass] = current
 	}
 	
-	inline fun <reified T> get(key: String): T? {
-		return classToEntry[T::class]?.get(key) as T?
+	inline fun <reified T> put(key: String, value: T) {
+		put(T::class, key, value)
 	}
+	
+	@Suppress("UNCHECKED_CAST")
+	fun <T>get(kclass: KClass<*>, key: String): T? = classToEntry[kclass]?.get(key) as T?
+	
+	inline fun <reified T> get(key: String): T? = get(T::class, key)
 	
 	fun copy(): Extras = Extras(
 		classToEntry.map { entry ->

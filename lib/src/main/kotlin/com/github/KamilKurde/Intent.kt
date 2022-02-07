d64296837ac8fb8e000dea1393a9d4b21472435a
Kamil Bąk <kamilbak113@gmail.com>
2022-02-07 17:34:04 +0100

    putExtra returns intent for inline Intent declaration

--- lib/src/main/kotlin/com/github/KamilKurde/Intent.kt
+++ lib/src/main/kotlin/com/github/KamilKurde/Intent.kt
@@ -5,7 +5,10 @@ import kotlin.reflect.KClass
 @Suppress("unused")
 data class Intent(val activity: KClass<out Activity>, val extras: Extras = Extras()) {
 	
-	inline fun <reified T> putExtra(key: String, value: T) = extras.put(key, value)
+	inline fun <reified T> putExtra(key: String, value: T): Intent {
+		extras.put(key, value)
+		return this
+	}
 	
 	inline fun <reified T : Any> getExtra(key: String): T? = extras.get(key)
 	
